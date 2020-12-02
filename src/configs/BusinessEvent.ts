ce7354f11d5071fbbb666d00127d2512aaaca4b4
Aashish Maheswar <Aashish.Maheswar@synechron.com>
2020-12-02 23:08:12 +0530

    yup - trade model id

--- src/configs/BusinessEvent.ts
+++ src/configs/BusinessEvent.ts
@@ -2,6 +2,6 @@ import * as yup from "yup";
 
 export const BusinessEventValidationSchema = yup.object().shape({
     businessEventName: yup.string().required("Business Event Name is required"),
-    // tradeModelId: yup.string().required("Trade Model Id is required"),
+    tradeModelId: yup.string().required("Trade Model Id is required"),
     triggerCondition: yup.string().required("Trigger Condition is required")
 });
