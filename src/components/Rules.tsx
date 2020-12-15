5ac4234a5c201ba7fa73769140f6ebe38f5ecbf0
Aashish Maheswar <Aashish.Maheswar@synechron.com>
2020-12-15 23:16:11 +0530

    attribute section - field array

--- src/components/Rules.tsx
+++ src/components/Rules.tsx
@@ -1,22 +1,22 @@
 import React from "react";
 import Box from "@material-ui/core/Box";
 import {
-  useFormik,
   Formik,
   Form,
-  Field,
   FieldArray,
-  ErrorMessage,
+  // Field,
+  // FieldArray,
+  // ErrorMessage,
+  getIn,
 } from "formik";
-import RuleQueryBuilder from "../core/components/QueryBuilder";
-import BusinessEventTriggerConfig from "../configs/BusinessEventTriggerConfig";
 import {
   Button,
-  Input,
-  InputLabel,
+  FormHelperText,
+  // Input,
+  // InputLabel,
   makeStyles,
   TextField,
-  Tooltip,
+  // Tooltip,
   Typography,
 } from "@material-ui/core";
 import Autocomplete from "@material-ui/lab/Autocomplete";
@@ -31,100 +31,160 @@ const useStyles = makeStyles({
 
 const ruleTypes = [{ id: "REG", name: "Regular" }];
 
+const initialValues = {
+  ruleId: "",
+  ruleName: "",
+  ruleType: null,
+  attributes: [{ name: "", type: "", id: Math.random() }],
+};
+
 const Rules = ({ isNew = false }: { isNew: boolean }) => {
   const classes = useStyles();
-  const formik = useFormik({
-    // initial values from API
-    initialValues: {
-      ruleId: "",
-      ruleName: "",
-      ruleType: null,
-    },
-    validationSchema: RuleModelValidationSchema,
-    // validateOnBlur: false,
-    onSubmit: (values) => {
-      const {
-        ruleType: { id: ruleType },
-      } = values as any;
-      let postData: any = { ...values, ruleType };
-      if (isNew) {
-        delete postData.ruleId;
-      }
-      alert(JSON.stringify(postData, null, 2));
-    },
-  });
 
   return (
     <Box width="80%" margin="auto">
       <Typography variant="h6" component="h6">
         {isNew ? "Create new Rule" : "Show/Edit Rule"}
       </Typography>
-      <form onSubmit={formik.handleSubmit}>
-        {!isNew && (
-          <TextField
-            margin="dense"
-            label="Rule Id"
-            InputLabelProps={{
-              shrink: true,
-            }}
-            name="ruleId"
-            fullWidth
-            disabled
-            value={formik.values.ruleId}
-            onBlur={formik.handleBlur}
-            onChange={formik.handleChange}
-          />
-        )}
-        <Autocomplete
-          // id="combo-box-demo"
-          fullWidth
-          value={formik.values.ruleType as any}
-          onChange={(event: any, newValue: any | null) => {
-            formik.setFieldValue("ruleType", newValue);
-          }}
-          options={ruleTypes} // fetched asynchronously; maybe elastic search
-          getOptionLabel={({ id, name }: { id: string; name: string }) =>
-            `${id} : ${name}`
+      <Formik
+        initialValues={initialValues}
+        validationSchema={RuleModelValidationSchema}
+        onSubmit={(values, { resetForm, setSubmitting }) => {
+          setSubmitting(true);
+          const {
+            ruleType: { id: ruleType },
+          } = values as any;
+          let postData: any = { ...values, ruleType };
+          if (isNew) {
+            delete postData.ruleId;
           }
-          renderInput={(params: any) => (
+          postData.attributes = postData.attributes.map(
+            ({ id, ...rest }: any) => rest
+          );
+          alert(JSON.stringify(postData, null, 2));
+          resetForm();
+          setSubmitting(false);
+        }}
+      >
+        {({
+          values,
+          touched,
+          errors,
+          handleChange,
+          handleBlur,
+          isValid,
+          setFieldValue,
+          getFieldProps,
+          isSubmitting,
+        }) => (
+          <Form>
+            {!isNew && (
+              <TextField
+                margin="dense"
+                label="Rule Id"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                fullWidth
+                disabled
+                {...getFieldProps("ruleId")}
+              />
+            )}
+            <Autocomplete
+              // id="combo-box-demo"
+              fullWidth
+              value={values.ruleType as any}
+              onChange={(event: any, newValue: any | null) => {
+                setFieldValue("ruleType", newValue);
+              }}
+              options={ruleTypes} // fetched asynchronously; maybe elastic search
+              getOptionLabel={({ id, name }: { id: string; name: string }) =>
+                `${id} : ${name}`
+              }
+              renderInput={(params: any) => (
+                <TextField
+                  {...params}
+                  required
+                  label="Rule Type"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+              )}
+            />
+            <br />
             <TextField
-              {...params}
               required
-              label="Rule Type"
+              margin="dense"
+              label="Rule Name"
               InputLabelProps={{
                 shrink: true,
               }}
+              fullWidth
+              {...getFieldProps("ruleName")}
+              error={touched.ruleName && Boolean(errors.ruleName)}
+              helperText={touched.ruleName && errors.ruleName}
+              className={classes.ruleName}
             />
-          )}
-        />
-        <br />
-        <TextField
-          required
-          margin="dense"
-          label="Rule Name"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          name="ruleName"
-          fullWidth
-          value={formik.values.ruleName}
-          onBlur={formik.handleBlur}
-          onChange={formik.handleChange}
-          error={formik.touched.ruleName && Boolean(formik.errors.ruleName)}
-          helperText={formik.touched.ruleName && formik.errors.ruleName}
-          className={classes.ruleName}
-        />
-        <br />
-        {/* placeholder for adding rule attributes */}
-        <h4>Attributes</h4>
-        <ul style={{ listStyle: "none" }}></ul>
-        <Button>Add Attribute</Button>
-        <Box>
-          <Button color="primary" type="submit">
-            Submit
-          </Button>
-        </Box>
-      </form>
+            <br />
+            {/* placeholder for adding rule attributes */}
+            <h4>Attributes</h4>
+            <FieldArray name="attributes">
+              {({ push, remove }) => {
+                return (
+                  <>
+                    <Box style={{ display: "flex", flexDirection: "column" }}>
+                      {values.attributes.map((attribute, idx) => {
+                        const name = `attributes[${idx}].name`;
+                        const touchedName = getIn(touched, name);
+                        const errorName = getIn(errors, name);
+
+                        const type = `attributes[${idx}].type`;
+                        const touchedType = getIn(touched, type);
+                        const errorType = getIn(errors, type);
+
+                        return (
+                          <Box key={attribute.id} display="flex">
+                            <TextField
+                              margin="dense"
+                              label="Name"
+                              InputLabelProps={{
+                                shrink: true,
+                              }}
+                              // disabled
+                              {...getFieldProps(name)}
+                              error={!!(touchedName && errorName)}
+                              helperText={touchedName && errorName}
+                            />
+                            <Button onClick={() => remove(idx)}>Remove</Button>
+                          </Box>
+                        );
+                      })}
+                    </Box>
+                    {values.attributes.length === 0 && (
+                      <FormHelperText error={Boolean(errors?.attributes)}>
+                        {errors?.attributes}
+                      </FormHelperText>
+                    )}
+                    <Button
+                      onClick={() =>
+                        push({ name: "", type: "", id: Math.random() })
+                      }
+                    >
+                      Add Attribute
+                    </Button>
+                  </>
+                );
+              }}
+            </FieldArray>
+            <Box>
+              <Button color="primary" type="submit" disabled={isSubmitting}>
+                Submit
+              </Button>
+            </Box>
+          </Form>
+        )}
+      </Formik>
     </Box>
   );
 };
--- src/configs/RuleModel.ts
+++ src/configs/RuleModel.ts
@@ -2,6 +2,12 @@ import * as yup from "yup";
 
 export const RuleModelValidationSchema = yup.object().shape({
     ruleName: yup.string().required("Rule Name is required"),
-    ruleType: yup.string().required("Rule Type is required")
+    ruleType: yup.string().required("Rule Type is required"),
+    attributes: yup.array().of(
+        yup.object().shape({
+            name: yup.string().required('Attribute name is required')
+        })
+    ).min(1, 'Atleast one attribute is needed')
     // triggerCondition: yup.string().required("Trigger Condition is required")
+    // .required('Attributes are required')
 });
